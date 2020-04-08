87184074814ae1fe957a91311c0dbbbe69dd1cd5
vitor sinedino <vitorsinedino@gmail.com>
2020-04-08 11:30:28 -0300

    Updates

---
+++ precache-manifest.b1c7d590c70257f055e38d63cf9ba90b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbf7bd3c6897f0de3bad45382cd7cfa",
+    "url": "/MonsterGenerator/index.html"
+  },
+  {
+    "revision": "96e5f45ee3d954fe6e36",
+    "url": "/MonsterGenerator/static/css/main.3f593bef.chunk.css"
+  },
+  {
+    "revision": "65dc3b08fbb08df5d13d",
+    "url": "/MonsterGenerator/static/js/2.3b8d7bc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterGenerator/static/js/2.3b8d7bc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96e5f45ee3d954fe6e36",
+    "url": "/MonsterGenerator/static/js/main.84480645.chunk.js"
+  },
+  {
+    "revision": "dcdafb5d8f9106f37046",
+    "url": "/MonsterGenerator/static/js/runtime-main.733cc4b6.js"
+  }
+]);
